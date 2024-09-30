2ee40d8550cf5d0a899d14ca51e531dfb1d0d218
rokas <rokas@rokas.com>
2024-09-30 18:48:18 +0200

    Update search screen UI with improved product display

--- app/search/index.tsx
+++ app/search/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Image, TouchableOpacity, Linking } from 'react-native';
 
 import dummyPproducts from '~/assets/search.json';
 const SearchResultScreen = () => {
@@ -9,11 +9,18 @@ const SearchResultScreen = () => {
     <View>
       <FlatList
         data={products}
+        contentContainerClassName="gap-2 rounded-lg p-4"
         renderItem={({ item }) => (
-          <View className="flex flex-row items-center justify-between p-2">
-            <Text className="text-lg">{item.name}</Text>
-            <Text className="text-lg">{item.price}</Text>
-          </View>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => Linking.openURL(item.url)}
+            className="flex flex-row items-center justify-between rounded-md bg-white p-2">
+            <Image source={{ uri: item.image }} className="mr-3 h-20 w-20 rounded-md" />
+            <Text className="flex-1 text-sm" numberOfLines={4}>
+              {item.name}
+            </Text>
+            <Text className="text-md">${item.final_price}</Text>
+          </TouchableOpacity>
         )}
         keyExtractor={(item) => item.id}
       />

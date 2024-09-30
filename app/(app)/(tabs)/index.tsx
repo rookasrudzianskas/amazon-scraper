573df1c307a9c2d9c6036778c8e25772c71edb14
rokas <rokas@rokas.com>
2024-09-30 21:25:30 +0200

    Add dayjs for date formatting in search components

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,15 +1,45 @@
 import { Ionicons } from '@expo/vector-icons';
-import { Stack, useRouter } from "expo-router";
-import { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { supabase } from "~/utils/supabase";
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { Stack, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { useAuth } from '~/contexts/AuthContext';
+import { supabase } from '~/utils/supabase';
+
+dayjs.extend(relativeTime);
 
 export default function Home() {
   const [search, setSearch] = useState('');
   const router = useRouter();
-  const performSearch = () => {
+  const { user } = useAuth();
+
+  const [pastHistory, setPastHistory] = useState([]);
+
+  useEffect(() => {
+    supabase
+      .from('searches')
+      .select('*')
+      .eq('user_id', user.id)
+      .then(({ data }) => {
+        setPastHistory(data);
+      });
+  });
+
+  const performSearch = async () => {
     console.log('Searching...');
-    router.push(`/search`);
+
+    // save in the database now
+    const { data } = await supabase
+      .from('searches')
+      .insert({
+        query: search,
+        user_id: user.id,
+      })
+      .select()
+      .single();
+    if (data) router.push(`/search/${data.id}`);
   };
 
   return (
@@ -34,6 +64,20 @@ export default function Home() {
       <TouchableOpacity onPress={() => supabase.auth.signOut()} className="m-2">
         <Text className="text-center">Sign out</Text>
       </TouchableOpacity>
+
+      <FlatList
+        data={pastHistory}
+        contentContainerStyle={{ padding: 10 }}
+        renderItem={({ item }) => (
+          <View className="my-1 flex flex-col items-start border-b border-black/20 p-2">
+            <Text className="font-semibold">{item.query}</Text>
+            <Text className="text-xs">
+              {dayjs(item.created_at).fromNow()}
+              <Text className="text-xs text-gray-500"> | Last updated</Text>
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
---
+++ app/(app)/search/[id].tsx
@@ -0,0 +1,78 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Linking,
+  ActivityIndicator,
+} from 'react-native';
+
+import dummyPproducts from '~/assets/search.json';
+import { supabase } from '~/utils/supabase';
+
+dayjs.extend(relativeTime);
+
+const SearchResultScreen = () => {
+  const products = dummyPproducts.slice(0, 20);
+  const { id } = useLocalSearchParams();
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    supabase
+      .from('searches')
+      .select('*')
+      .eq('id', id)
+      .single()
+      .then(({ data }) => {
+        setSearch(data);
+      });
+  }, [id]);
+
+  if (!search) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <View className="m-2 rounded bg-white p-2 shadow-sm">
+        <Text className="text-lg">{search.query}</Text>
+        <Text className="text-sm">
+          {dayjs(search.created_at).fromNow()}
+          <Text className="text-xs text-gray-500"> | Last updated</Text>
+        </Text>
+        <Text className="text-sm text-gray-500">{dayjs(search.updated_at).fromNow()}</Text>
+      </View>
+      <FlatList
+        data={products}
+        contentContainerClassName="gap-2 rounded-lg p-4"
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => Linking.openURL(item.url)}
+            className="flex flex-row items-center justify-between rounded-md bg-white p-2">
+            <Image source={{ uri: item.image }} className="mr-3 h-20 w-20 rounded-md" />
+            <Text className="flex-1 text-sm" numberOfLines={4}>
+              {item.name}
+            </Text>
+            <Text className="text-md">${item.final_price}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.asin}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default SearchResultScreen;

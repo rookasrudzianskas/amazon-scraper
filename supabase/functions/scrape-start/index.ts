fc1dfb0591a37a74d884d5074fd3313e3528500d
rokas <rokas@rokas.com>
2024-09-30 22:11:52 +0200

    Add function to start scraping process

--- supabase/functions/scrape-start/index.ts
+++ supabase/functions/scrape-start/index.ts
@@ -5,58 +5,57 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
 import { createClient } from 'jsr:@supabase/supabase-js@2';
 console.log('Hello from Functions!');
-
-Deno.serve(async (req) => {
-  const url = new URL(req.url);
-  const id = url.searchParams.get('id');
-  const reqJson = await req.json();
-  const authHeader = req.headers.get('Authorization')!;
-  const supabase = createClient(
-    Deno.env.get('SUPABASE_URL') ?? '',
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+// curl
+// -H "Authorization: Bearer 6aa19c1e-d9f9-4209-9854-9871a293340b"
+// -H "Content-Type: application/json"
+// -d '[{"keyword":"X-box","url":"https://www.amazon.com","pages_to_search":1}]'
+// "https://api.brightdata.com/datasets/v3/trigger?dataset_id=gd_lwdb4vjm1ehb499uxs&limit_multiple_results=10"
+const startScraping = async (keyword: string, search_id: string) => {
+  const searchParams = new URLSearchParams({
+    dataset_id: 'gd_lwdb4vjm1ehb499uxs',
+    format: 'json',
+    uncompressed_webhook: 'true',
+    limit_multiple_results: '10',
+    endpoint: `https://ttxvnyflpzstmpdrbtnz.supabase.co/functions/v1/scrape-complete?id=${search_id}`,
+    auth_header: `Bearer ${Deno.env.get('SUPABASE_ANON_KEY')}`,
+  });
+  const res = await fetch(
+    `https://api.brightdata.com/datasets/v3/trigger?${searchParams.toString()}`,
     {
-      global: {
-        headers: {
-          Authorization: `Bearer ${Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')}`,
-        },
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Deno.env.get('BRIGHT_DATA_API_KEY')}`,
       },
+      body: JSON.stringify([
+        {
+          keyword,
+          url: 'https://www.amazon.com',
+          pages_to_search: 1,
+        },
+      ]),
     }
   );
-  const updated_at = new Date().toISOString();
-  // save all products
-  const products = reqJson.map((p) => ({
-    asin: p.asin,
-    updated_at,
-    name: p.name,
-    image: p.image,
-    url: p.url,
-    final_price: p.final_price,
-    currency: p.currency,
-  }));
-  const { error: productsError } = await supabase.from('products').upsert(products);
-  console.log(productsError);
-  // link products with search ids.
-  const productSearchLinks = products.map((p) => ({
-    asin: p.asin,
-    search_id: id,
-  }));
-  // Might insert extra links when a product is updated
-  const { error: product_searchError } = await supabase
-    .from('product_search')
-    .upsert(productSearchLinks);
-  console.log(product_searchError);
-  // update search status
-  const { error: searchesError } = await supabase
+  const resJson = await res.json();
+  return resJson;
+};
+Deno.serve(async (req) => {
+  const { record } = await req.json();
+  const newScrape = await startScraping(record.query, record.id);
+  console.log(newScrape);
+  const authHeader = req.headers.get('Authorization')!;
+  const supabaseClient = createClient(
+    Deno.env.get('SUPABASE_URL') ?? '',
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+    { global: { headers: { Authorization: authHeader } } }
+  );
+  const { data, error } = await supabaseClient
     .from('searches')
-    .update({
-      status: 'Done',
-      last_scraped_at: updated_at,
-    })
-    .eq('id', id);
-  console.log(searchesError);
-  const data = {
-    message: `Hello!`,
-  };
+    .update({ snapshot_id: newScrape.snapshot_id, status: 'Scraping' })
+    .eq('id', record.id)
+    .select()
+    .single();
+  console.log(error);
   return new Response(JSON.stringify(data), {
     headers: { 'Content-Type': 'application/json' },
   });
@@ -64,8 +63,13 @@ Deno.serve(async (req) => {
 /* To invoke locally:
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/scrape-complete' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/scrape-start' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"record": {"id": 1, "query": "IPhone"}}'
+  --- Remote request
+  curl -i --location --request POST 'https://ttxvnyflpzstmpdrbtnz.supabase.co/functions/v1/scrape-start' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0eHZueWZscHpzdG1wZHJidG56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NDcwMDcsImV4cCI6MjA0MzAyMzAwN30.a4o5I6vgMR-OevvUcsud12KvdSkYY_iYa6oOz2wlg10' \
+    --header 'Content-Type: application/json' \
+    --data '{"record": {"id": "1", "query": "IPhone"}}'
 */
